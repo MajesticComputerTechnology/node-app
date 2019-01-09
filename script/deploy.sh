eec7c5958eb58c00e8d9337130810f7d5da3b3b3
ivan@majestic.com.au <ivan@majestic.com.au>
2019-01-10 10:38:23 +1100

    node-modules and script changes

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-cat <<EOF
-  cd /var/www/html/node-app
-  git pull
-  npm install --production
-  pm2 restart all
-  exit
-EOF
+ cd /var/www/html/node-app
+ git pull
+ npm install --production
+ pm2 restart all
+ exit
